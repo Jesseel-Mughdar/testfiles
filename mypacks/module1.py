c26812fc19258c264b1285a0d991973af109e9cb
Jesseel Mughdar <98876505+Jesseel-Mughdar@users.noreply.github.com>
2022-02-04 14:12:40 +0530

    Add files via upload

---
+++ mypacks/module1.py
@@ -0,0 +1,6 @@
+def function1():
+	print("Package-=> Module1--> fucntions1")
+def function12():
+	print("Package-=> Module1--> fucntions2")	
+def function3():
+	print("Package-=> Module1--> fucntions3")
