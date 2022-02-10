504f5a986ac4ad615302eb05006efdd5b6ff573c
jesseel <Jesseel@freshkart.io>
2022-02-10 07:45:48 +0530

    committed

--- apipack2/main.py
+++ apipack2/main.py
@@ -9,20 +9,10 @@ def root():
 
 @app.get("/getitem/{n}")
 def inp(n:int):
-    for i in range(0, len(list)):
-        if i==n:
-            return{'item not found'}   
-           
-        else:
-            user_saved = Fibonacci(n)
-            return user_saved
-             
+    user_saved = Fibonacci(n)
+    return user_saved
+                  
 
-     
-@app.post("/putitem/{n}")
-def get_data(n:int):
-    list.append(n)
-    return list
 
 if __name__=='__main__':
     uvicorn.run(app)      
